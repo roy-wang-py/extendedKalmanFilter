c9c6e5d4c77427e8011a29776182cbd2c36e020c
roy-wang-py <wpyroy@qq.com>
2018-10-14 17:03:29 +0800

    Finished tools.cpp(calculate jacobian & calculate rmse);finished kalman_filter.cpp(preddict & update & updateekf);finished fusiionekf.cpp(achive externed kalman filter)

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -36,8 +36,29 @@ FusionEKF::FusionEKF() {
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
   */
+  H_laser_ << 1,0,0,0,
+          0,1,0,0;
+  
+  ekf_.F_ = MatrixXd(4,4);
+  ekf_.P_ = MatrixXd(4,4);
+  ekf_.Q_ = MatrixXd(4,4);
 
+  ekf_.F_ << 1,0,1,0,
+        0,1,0,1,
+        0,0,1,0,
+        0,0,0,1;
 
+  ekf_.P_ << 1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+
+  ekf_.Q_ << 0,0,0,0,
+        0,0,0,0,
+        0,0,0,0,
+        0,0,0,0;
+  previous_timestamp_ = 0;
+  cout<<"inital class fusionEKF"<<endl;
 }
 
 /**
@@ -46,7 +67,6 @@ FusionEKF::FusionEKF() {
 FusionEKF::~FusionEKF() {}
 
 void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
-
 
   /*****************************************************************************
    *  Initialization
@@ -58,6 +78,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       * Create the covariance matrix.
       * Remember: you'll need to convert radar from polar to cartesian coordinates.
     */
+
     // first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
@@ -66,14 +87,19 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
-      */
+      */   
+      double x_rate = sin(measurement_pack.raw_measurements_[1]);
+      double y_rate = cos(measurement_pack.raw_measurements_[1]);
+      //ekf_.x_ << measurement_pack.raw_measurements_[0]*x_rate, measurement_pack.raw_measurements_[0]*y_rate,measurement_pack.raw_measurements_[2]*x_rate, measurement_pack.raw_measurements_[2]*y_rate;
+      ekf_.x_ << measurement_pack.raw_measurements_[0]*x_rate, measurement_pack.raw_measurements_[0]*y_rate,0, 0;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
+      ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1],0,0;
     }
-
+	previous_timestamp_ = measurement_pack.timestamp_;
     // done initializing, no need to predict or update
     is_initialized_ = true;
     return;
@@ -90,6 +116,25 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
+  //update dt
+  double dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+  previous_timestamp_ = measurement_pack.timestamp_;
+  ekf_.F_ << 1,0,dt,0,
+          0,1,0,dt,
+          0,0,1,0,
+          0,0,0,1;
+  
+  //update Q matrix
+	double noise_ax = 9.0;
+	double noise_ay = 9.0;  
+	double dt_2 = dt*dt;
+	double dt_3 = dt_2*dt;
+	double dt_4 = dt_3*dt;
+	//kf_.Q_ = MatrixXd(4, 4);
+	ekf_.Q_ << (dt_4/4)*noise_ax, 0,                 (dt_3/2)*noise_ax, 0,
+			  0,                 (dt_4/4)*noise_ay, 0,                 (dt_3/2)*noise_ay,
+			  (dt_3/2)*noise_ax, 0,                 dt_2*noise_ax,     0,
+			  0,                 (dt_3/2)*noise_ay, 0,                 dt_2*noise_ay;  
 
   ekf_.Predict();
 
@@ -105,8 +150,18 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+
+    ekf_.R_ = R_radar_;
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+
   } else {
+
     // Laser updates
+    ekf_.R_ = R_laser_;
+    ekf_.H_ = H_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
+
   }
 
   // print the output
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,5 +1,7 @@
 #include "kalman_filter.h"
+#include <iostream>
 
+using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -18,6 +20,7 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
   H_ = H_in;
   R_ = R_in;
   Q_ = Q_in;
+
 }
 
 void KalmanFilter::Predict() {
@@ -25,6 +28,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +38,20 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;  
+  
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +59,30 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+    double c1 = x_[0]*x_[0]+x_[1]*x_[1];
+    double c2 = sqrt(c1);
+    if(fabs(c1) < 0.0001){
+        cout << "UpdateEKF () - Error - Division by Zero" << endl;
+        return ;
+    }
+
+    //VectorXd z_pred = H_ * x_;
+    VectorXd z_pred(3);
+    //angle = atan2(x_[1]/x_[0]);
+    z_pred<< c2, atan2(x_[1],x_[0]) , (x_[0]*x_[2] + x_[1]*x_[3])/c2;
+    VectorXd y = z - z_pred;
+    //cout<<"z[1]:"<<z[1]<<"  z_pred[1]:"<<z_pred[1]<<"y[1]:"<<y[1]<<endl;
+    y[1] = atan2(sin(y[1]), cos(y[1]));
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;  
+  
 }
